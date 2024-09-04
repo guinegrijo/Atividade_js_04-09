71359e4a81de740c52f05f5ee431fed2f3e7e7bf
guinegrijo <guilhermenegrijo@gmail.com>
2024-09-04 12:54:46 -0300

    Exercicio API

--- exercicio01.js
+++
@@ -1,8 +0,0 @@
-var idade = 18
-if(idade < 18 && idade >=0){
-    console.log("Menor de idade")
-} else if (idade >= 18){
-    console.log('Maior de idade')
-} else{
-    console.log('Digite uma idade natural')
-}
