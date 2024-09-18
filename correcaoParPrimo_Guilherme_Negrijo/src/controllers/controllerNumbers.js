6862ee790177a76d802c0ca35a621fe20939c0ff
guinegrijo <guilhermenegrijo@gmail.com>
2024-09-18 09:38:48 -0300

    Exercicio API

---
+++ correcaoParPrimo_Guilherme_Negrijo/src/controllers/controllerNumbers.js
@@ -0,0 +1,26 @@
+module.exports = class controllerNumbers {
+    static async checkPar(req, res){
+        const {numero} = req.body
+
+        if (numero % 2 == 0){
+            return res.status(200).json({message: "Número Par!"})
+        }
+        return res.status(400).json({message: "Número Ímpar!"})
+    }
+
+    static async checkPrimo(req, res) {
+        const {numero} = req.body
+
+        if (numero <= 1) {
+            return res.status(200).json({ message: 'O número não é primo.' })
+          }
+        
+          for (let i = 2; i < numero; i++) {
+            if (numero % i === 0) {
+              return res.status(200).json({ message: 'O número não é primo.' })
+            }
+          }
+        
+          return res.status(200).json({ message: 'O número é primo.' })
+    }
+}
